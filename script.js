adf765a701dedd3d12924f13dc249c09583a511b
aubin-pierre7 <aubinp58@gmail.com>
2026-01-03 15:32:00 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function compute()
+{
+    p = document.getElementById("principal").value;
+    
+}
+        
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
